c2d8aeb8ef85bc99b15dfa2c71df60efc7c8a871
Baljinder Hayre <bal@wearejh.com>
2020-07-30 22:15:08 +0100

    Fully commented CipherTest and removed redundant TextEntered and TextEnteredTest files

--- README.md
+++ README.md
@@ -9,7 +9,7 @@
   For example, 
   
 ```bash
-  | Text               | Key | Encrypt/ Decrypt | Result 	            |
+  | Text               | Key | Encrypt/ Decrypt | Result    	        |
   |------------------- |-----|------------------|-----------------------|
   |Hello there Zacy123 |  2  |    E             |  Jgnnq vjgtg Bcea123. |
   |Gqtb rj Ejs 2021!   |  5  |    D             |  Blow me Zen 2021!    |
@@ -17,17 +17,23 @@
   Due to this simplicity, the Caesar cipher offers little security against those with even a passing
   knowledge of cryptography. Although the Caesar cipher still has several applications today in a 
   variety of fields. This is due to its flexibility in acting as both a simple code for education 
-  and fun and as a building block for more complex encryptions:
+  and fun and as a building block for more complex encryptions.
   
 ## Running
- *   To run the program, with user input :- 
+ To run the program, with user enters in the
+ * Actual text which can be anything from a character to a full paragraph
+ * User key - the number of places the characters each need to move 
+ * User cipher type - either encrypt or decrypt 
+ 
+ 
+
 ```bash
     php index.php
 ```     
   
 ## Tests
 
-*    To run the unit tests, with predefined tests :-
+ To run the unit tests, with predefined tests :-
  
 ```bash
     composer run-cipher-test
--- Test/CipherTest.php
+++ Test/CipherTest.php
@@ -4,128 +4,166 @@ declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
 
-/** @test  */
-
+/** @test
+ *
+ * This module holds the unit tests that were carried out to ensure that the Caesar cipher works correctly.
+ * Each test requires the user to enter in the actual text to cipher, the key (the number of places each
+ * character needs to move) and the method of ciphering (Encryption  or Decryption).
+ * All characters that are not alphabetical are kept the same
+ *
+ */
 class CipherTest extends TestCase
 {
-    public function testBasicTest()
-    {
-        $this->assertTrue(true);
+    /**
+     * A basic test to ensure that the command to run the unit test works
+     *
+     */
+    public function testBasicTest(){
+        $this-> assertTrue(true);
     }
-
+    /**
+     * Test : to move all characters in one word forward at a time.
+     * Am NOT testing looping around Z with this test
+     *
+     */
     public function testOneWordMovesCorrectNumberForward(){
-      $userText ='Hell8e';
-      $expectedResult ="Jgnn8g";
-      $userKeys= 2;
-      $cipherType = "E";
-      $userInput = new \App\Cipher();
-      $this ->assertSame($expectedResult, $userInput->CiperText($userText,$userKeys,$cipherType));
+        $userText = 'Hell8e';
+        $expectedResult = 'Jgnn8g';
+        $userKeys = 2;
+        $cipherType = 'E';  //Encrypt
+        $userInput = new \App\Cipher();
+        $this -> assertSame($expectedResult, $userInput->CiperText($userText,$userKeys,$cipherType));
     }
-
+    /**
+     * Test : to move all words in a sentence forward.
+     * Am NOT testing looping around Z with this test
+     *
+     */
     public function testASentenceMovesForward(){
         $userText ='Hello Boo. Thats right!';
-        $expectedResult ="Jgnnq Dqq. Vjcvu tkijv!";
-        $userKeys= 2;
-        $cipherType = 'E';   //Decript
+        $expectedResult = 'Jgnnq Dqq. Vjcvu tkijv!';
+        $userKeys = 2;
+        $cipherType = 'E';   //Encrypt
         $userInput = new \App\Cipher();
-        $this ->assertSame($expectedResult, $userInput->CiperText($userText,$userKeys,$cipherType));
+        $this -> assertSame($expectedResult, $userInput->CiperText($userText,$userKeys,$cipherType));
     }
+    /**
+     * Test : to move all characters in a word forward, looping around the letter Z.
+     * For example if the input was 'Z' and the key was 2 then the loop would go from Z, A, to B
+     *
+     */
     public function testWordMovesAroundZ(){
-        $userText ='Xyz';
-        $expectedResult ="Zab";
-        $userKeys= 2;
-        $cipherType = 'E';   //Decript
+        $userText = 'Xyz';
+        $expectedResult = 'Zab';
+        $userKeys = 2;
+        $cipherType = 'E';   //Encrypt
         $userInput = new \App\Cipher();
-        $this ->assertSame($expectedResult, $userInput->CiperText($userText,$userKeys,$cipherType));
+        $this -> assertSame($expectedResult, $userInput->CiperText($userText,$userKeys,$cipherType));
     }
-    public function testSentenceMovesAroundZ(){
+    /**
+     * Test : to move all words in a sentence forward, loop around the letter Z
+     * For example if the input was 'Zip' and the key was 2 then the loop would go
+     * from Z, A, to B
+     * from i ,j to k
+     * from p , q to r
+     * so the final output should be Bkr
+     *
+     */
+    public function testSentenceMovesForwardAroundZ(){
         $userText ='Hello you.  Yes You Mr Zorby!';
-        $expectedResult ="Jgnnq aqw.  Agu Aqw Ot Bqtda!";
-        $cipherType = 'E';   //Decript
-        $userKeys= 2;
+        $expectedResult = 'Jgnnq aqw.  Agu Aqw Ot Bqtda!';
+        $cipherType = 'E';   //c
+        $userKeys = 2;
         $userInput = new \App\Cipher();
-        $this ->assertSame($expectedResult, $userInput->CiperText($userText,$userKeys,$cipherType));
+        $this -> assertSame($expectedResult, $userInput->CiperText($userText,$userKeys,$cipherType));
     }
-
+    /**
+     * Test : to move all characters in one word backward at a time.
+     * Am NOT testing looping around A with this test
+     *
+     */
     public function testOneWordMovesCorrectNumberBackwards(){
-        $userText ='Jgnnq ';
-        $expectedResult ="Hello ";
-        $userKeys= 2;
-        $cipherType = 'D';   //Decript
-
+        $userText = 'Jgnnq ';
+        $expectedResult = 'Hello ';
+        $userKeys = 2;
+        $cipherType = 'D';   //Decrypt
         $userInput = new \App\Cipher();
-        $this ->assertSame($expectedResult, $userInput->CiperText($userText,$userKeys,$cipherType));
+        $this -> assertSame($expectedResult, $userInput->CiperText($userText,$userKeys,$cipherType));
     }
+    /**
+     * Test : to move all characters in a word backward, looping around the letter A to Z.
+     * For example if the input was 'A' and the key was 2 then the loop would go from A, Z, to Y
+     *
+     */
     public function testWordMovesCorrectNumberBackwardsAroundZ(){
-        $userText ="c";
-        $expectedResult ="y";
-        $userKeys= 4;
-        $cipherType = 'D';   //Decript
+        $userText = 'c';
+        $expectedResult = 'y';
+        $userKeys = 4;
+        $cipherType = 'D';   //Decrypt
         $userInput = new \App\Cipher();
-        $this ->assertSame($expectedResult, $userInput->CiperText($userText,$userKeys,$cipherType));
+        $this -> assertSame($expectedResult, $userInput->CiperText($userText,$userKeys,$cipherType));
     }
-
+    /**
+     * Test : to move all words in a sentence backward, loop around the letter A
+     * For example if the input was 'Zic Abel' and the key was 2 then the loop would go
+     * from Z, X to Y
+     * from i ,h to g
+     * from p , o to n
+     * so the final output should be Ygn
+     * and then the next word - following the same steps
+     *
+     */
     public function testSentenceMovesCorrectNumberBackwardsAroundZ(){
-        $userText ="Yja co K fqkpi vjku pqy Bceegta?";
-        $expectedResult ="Why am I doing this now Zaccery?";
-        $userKeys= 2;
-        $cipherType = 'D';   //Decript
-
+        $userText = 'Yja co K fqkpi vjku pqy Bceegta?';
+        $expectedResult = 'Why am I doing this now Zaccery?';
+        $userKeys = 2;
+        $cipherType = 'D';   //Decrypt
         $userInput = new \App\Cipher();
-        $this ->assertSame($expectedResult, $userInput->CiperText($userText,$userKeys,$cipherType));
+        $this -> assertSame($expectedResult, $userInput->CiperText($userText,$userKeys,$cipherType));
     }
-
+    /**
+     * Test : to move all words in a sentence forward, loop around the letter A
+     * For example if the input was 'Zip Axy' and the key was 2 then the loop would go
+     * from Z, A, to B
+     * from i ,j to k
+     * from p , q to r
+     * so the final output should be Bkr
+     * and then the next word - following the same steps
+     *
+     */
     public function testSentenceMovesCorrectNumberforwardAroundZ(){
-        $userText ="The quick brown fox jumps over the lazy dog";
-        $expectedResult ="Vjg swkem dtqyp hqz lworu qxgt vjg ncba fqi";
-        $userKeys= 2;
-        $cipherType = 'E';   //Decript
+        $userText = 'The quick brown fox jumps over the lazy dog';
+        $expectedResult = 'Vjg swkem dtqyp hqz lworu qxgt vjg ncba fqi';
+        $userKeys = 2;
+        $cipherType = 'E';   //Encrypt
         $userInput = new \App\Cipher();
-        $this ->assertSame($expectedResult, $userInput->CiperText($userText,$userKeys,$cipherType));
-    }
-    public function testUserChoosingInputToDecipherText(){
-        $userText ="Vjg swkem dtqyp hqz lworu qxgt vjg ncba fqi";
-        $expectedResult ="The quick brown fox jumps over the lazy dog";
-
-        $userKeys= 2;
-        $cipherType = 'D';   //Decript
-
-        $userInput = new \App\Cipher();
-        $this ->assertSame($expectedResult, $userInput->CiperText($userText,$userKeys,$cipherType));
-
-    }
-    public function testUserChoosingInputToEncipherText(){
-        $expectedResult ="Cryptography";
-        $userText = "Fubswrjudskb";
-
-        $userKeys= 3;
-        $cipherType ='D';   //Encrpt
-
-        $userInput = new \App\Cipher();
-        $this ->assertSame($expectedResult, $userInput->CiperText($userText,$userKeys,$cipherType));
-
+        $this -> assertSame($expectedResult, $userInput->CiperText($userText,$userKeys,$cipherType));
     }
     /**
      * @dataProvider dataProviderForCipher
+     *
+     * Test : to move all words in a sentence forward or backward depending upon what is stored in the data module
+     *
      */
-    public function testBalkar($userText, $expectedResult, $userKeys,$cipherType ){
-
+    public function testUsingDataProviderToGiveData($userText, $expectedResult, $userKeys,$cipherType ){
         $userInput = new \App\Cipher();
         $this ->assertSame($expectedResult, $userInput->CiperText($userText,$userKeys,$cipherType));
-
     }
-    public function dataProviderForCipher()
-    {
+    /**
+     * Test : to store all the data needed for the unit tests in one module
+     *
+     */
+    public function dataProviderForCipher(){
         return [
-            "FirstText" => [$userText = "Fubswrjudskb",
-                            $expectedResult ="Cryptography",
-                             $userKeys= 3,
-                             $cipherType ='D'],
+            "FirstText" => [$userText = 'Fubswrjudskb',
+                            $expectedResult = 'Cryptography',
+                             $userKeys = 3,
+                             $cipherType = 'D'],
 
-              "SecondText" => [ $userText ='Hello Boo. Thats right!',
-                                $expectedResult ="Jgnnq Dqq. Vjcvu tkijv!",
-                                $userKeys= 2,
-                                $cipherType ='E']
+              "SecondText" => [ $userText = 'Hello Boo. Thats right!',
+                                $expectedResult = 'Jgnnq Dqq. Vjcvu tkijv!',
+                                $userKeys = 2,
+                                $cipherType = 'E']
         ];
     }
 }
